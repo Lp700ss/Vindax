9e378e7d1a91d828c748465a324a7668d16683ff
Lp700s <siddhant@studio137.co.za>
2022-08-17 18:27:24 +0530

    adding Dashboard

---
+++ components/Dashboard/Crypto/Watchlist.js
@@ -0,0 +1,12 @@
+import { MouseEvent, useState } from 'react';
+import {
+  Button,
+  Box,
+  ToggleButton,
+  ToggleButtonGroup,
+  Card,
+  Typography,
+  styled
+} from '@mui/material';
+import ViewWeekTwoToneIcon from '@mui/icons-material/ViewWeekTwoTone';
+import TableRowsTwoToneIcon from '@mui/icons-material/TableRowsTwoTone';
